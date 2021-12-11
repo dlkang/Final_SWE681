468145c69ecbb177aa061432f5e4afb892d5ee74
H1j4ck3d <47256546+H1j4ck3d@users.noreply.github.com>
2021-12-11 04:27:07 -0500

    SocketIO test commit

--- flask_app/adventureGame.py
+++ flask_app/adventureGame.py
@@ -1,58 +1,182 @@
-from flask import Blueprint, redirect, render_template, request, url_for, flash, jsonify
-from flask_app.game import grid_map
+
+from flask import session,Blueprint, redirect, render_template, request, url_for, flash, jsonify
+from flask_app.game import grid_map, Room, Player
+from flask_app import db
+from flask_app.models import Account, Game, Hero
+from flask_app.forms import HeroForm
+from flask_login import current_user
+from flask_socketio import SocketIO, join_room, leave_room, close_room, emit, send
 import json
-from werkzeug import security
-from __main__ import *
+from datetime import datetime
+import time
+from uuid import uuid4
 
-bp = Blueprint('game', __name__, url_prefix='/game')
 
-@socketio.on('connect')
+bp = Blueprint('game', __name__, url_prefix='/play')
+
+socketio = SocketIO()
+
+# List of active games to join
+rooms = {}
+
+@bp.route('/newgame')
+def create_game():
+    if not current_user.is_authenticated:
+        flash('You need to be logged in to create a game')
+        return redirect(url_for("auth.login"))
+
+    #create game identifier and check it doesnt exist
+    id = str(uuid4().int)
+    if id in rooms:
+        raise Exception("Games with the same ID cant exist")
+
+    rooms[id] = Room(id)
+
+    #Redirect to the room
+    return redirect(url_for('game.room', id=id))
+
+@bp.route('/joingame')
+def join_game():
+    if not current_user.is_authenticated:
+        flash('You need to be logged in to join a game')
+        return redirect(url_for("auth.login"))
+
+    # Get a list of all the rooms ready to be joined
+    ready = []
+
+    for room in rooms.values():
+        if room.isReady():
+            ready.append((room.id, room.getPlayers()))
+
+    return render_template('lobby.html', rooms=ready)
+
+
+# Game room function: where the game occurs
+@bp.route('/game/<id>')
+def game(id):
+    if not current_user.is_authenticated:
+        flash('You need to login first.')
+        return redirect(url_for('auth.login'))
+
+    # Gets the room object from the active rooms dictionary
+    room = rooms.get(id)
+
+    # Back to index if the room doesnt exist anymore
+    if room is None:
+        flash('Game not found.')
+        return redirect(url_for('index'))
+
+    # Saves the room in the session
+    session['room'] = id
+
+    # Add the new player to the room
+    if current_user.username not in room.getPlayers():
+        # Unless the room is full
+        if room.isFull():
+            flash('The game is full. Try another one.')
+            return redirect(url_for('play.join_game'))
+        room.addPlayer(current_user.username)
+
+    return render_template('game.html')
+
+
+# Socket connection event
+@socketio.on('connect', namespace='/room')
 def new_connection():
-    print('Socket Connected')
+    if not current_user.is_authenticated:
+        return False
 
-@socketio.on('message')
-def handleMessage(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast=True)
+    # Gets the room and player where the client is at from the session
+    room = rooms.get(session['room'])
+    player = room.getByName(current_user.username)
 
-@bp.route('/start', methods=['GET'])
-def start_game():
+    # Does nothing if the player has not already been added to the room
+    # or if the player is already connected
+    if player is None or player in room.connected:
+        return False
+
+    # Stores the connection session_id for the player
+    room.connect(player, request.sid)
+
+    # Adds the player to the room so that it receives events
+    join_room(room)
+
+    # If player was just inactive
+    if room.isStarted():
+        return True
+
+    # Sends connection to all players in room
+    send("User {} has connected.".format(player.name), namespace='/room', room=room)
+
+    # If room has all players, start the game
+    if room.isFull():
+        start(room)
+    return
+
+
+# Socket disconnection event
+@socketio.on('disconnect', namespace='/room')
+def new_disconnection():
+
+    room = rooms.get(session['room'])
+    if room is None:
+        return
+
+    #Obtain the player identity
+    player = room.getBySid(request.sid)
+    if player is None:
+        return
+
+    # The player is removed from the connected players and from room
+    room.disconnect(player)
+    leave_room(room)
+
+    # If the game has started
+    if room.isStarted():
+        # It waits 60 seconds for a reconnection
+        time.sleep(60)
+
+        # If after that time the player has not reconnected
+        if player not in room.connected:
+            send("User {} was afk for too long.".format(player.name), namespace='/room', room=room)
+
+            # TODO The player loses the game IMPLEMENT IT
+            player.health = 0
+            # The game ends
+            finish(room)
+            return
+        return
+
+    # If the game hasn't started, the player is removed from the players list
+    room.players.remove(player)
+    send("User {} has disconnected.".format(player.name), namespace='/room', room=room)
+
+    # If there are no players in a room, game is removed
+    if len(room.players) == 0:
+        rooms.pop(session['room'])
+    return
+
+
+#Game start
+def start(room):
+    #Map is created
     grid = grid_map()
     grid_json = json.dumps(grid)
-    data = {'grid': grid_json}
-    return render_template("game.html", title='PLAY', data=data)
+    map = {'grid': grid_json}
 
-@bp.route('/sendmove', methods=['POST'])
-def validate_move():
-    output_json = dict()
+    #Database is initialized
+    start_db(room)
 
-    data = request.get_json()
-    reqKeys = {'player_id', 'attack', 'posX', 'posY'}
-    if set(data.keys()) == reqKeys:
-        print('Keys Match')
-    else:
-        # print(set(data.keys()))
-        # print(reqKeys)
-        return 'Invalid Move'
+    # Every player receives the names of other players
+    # and his team (based on his possition entering the room)
+    for p in room.players:
+        data = {
+            "scoreBlue": room.scoreBlue,
+            "scoreRed": room.scoreRed,
+            "players": room.getPlayers(),
+            "player_number": room.players.index(p)
+        }
+        emit('start_game', data, namespace='/game', room=p.sid)
+    new_round(room)
+    return
 
-    output_json['player_id'] = data['player_id']
-
-    if int(data['attack']) >= 0 and int(data['attack']) <= 100:
-        output_json['attack'] = 'VALID'
-    else:
-        output_json['attack'] = 'INVALID'
-
-    if int(data['posX']) >= 0 and int(data['posX']) <= 10:
-        output_json['posX'] = 'VALID'
-    else:
-        output_json['posX'] = 'INVALID'
-
-    if int(data['posY']) >= 0 and int(data['posY']) <= 10:
-        output_json['posY'] = 'VALID'
-    else:
-        output_json['posY'] = 'INVALID'
-    print("Data: ")
-    print(data)
-    print("OutputJson: ")
-    print(output_json)
-    return jsonify(output_json)
--- flask_app/static/main.js
+++ flask_app/static/main.js
@@ -1,3 +1,9 @@
+var socket = io()
+socket.on('connect', function() {
+    socket.emmit('my event', {data:'Im connected!'})
+})
+
+
 function get_grid(){
     return graphData;
 }
@@ -23,8 +29,45 @@ var column = row.selectAll(".square")
 	.attr("y", function(d) { return d.y; })
 	.attr("width", function(d) { return d.width; })
 	.attr("height", function(d) { return d.height; })
-	.style("fill", "#fff")
-	.style("stroke", "#222")
-	.on('click', function(d) {
-	        console.log("Click!");
-	        d3.select(this).style("background-color", "#f9886c")})
+	.attr("fill", function(d) {
+	    return (d.hero > 0) ? "#0000ff" : "#fff";
+	})
+	.attr("stroke", "#222")
+
+
+$("button").click(function(){
+    var direction = $(this).val();
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "checkmove");
+    xhttp.send(direction);
+});
+
+
+  $('#up').click(function(){
+      jsonToSend = {bid: myBid, see: false};
+      socket.emit('client_game_turn',jsonToSend);
+  });
+  $('#down').click(function(){
+      jsonToSend = {bid: myBid, see: false};
+      socket.emit('client_game_turn',jsonToSend);
+  });
+  $('#left').click(function(){
+      jsonToSend = {bid: myBid, see: false};
+      socket.emit('client_game_turn',jsonToSend);
+  });
+  $('#right').click(function(){
+      jsonToSend = {bid: myBid, see: false};
+      socket.emit('client_game_turn',jsonToSend);
+  });
+  $('#attack').click(function(){
+      jsonToSend = {bid: myBid, see: false};
+      socket.emit('client_game_turn',jsonToSend);
+  });
+
+
+
+/**
+var paragraph = document.getElementById("log_window");
+var text = document.createTextNode(info);
+paragraph.appendChild(text)
+var info = {{info | safe}} **/
